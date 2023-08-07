fd00d5237eb54119bfc39ea49a27a40b78112831
Don Ngo <105596803+Don-Tat@users.noreply.github.com>
2023-08-07 17:43:42 -0400

    Fundamentals part 4 assignment completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let celsius = (temperature - 32) * (5/9);
+  return
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = (temperature * (9/5)) + 32;
+  return fahrenheit;
 };
 
 // Do not edit below this line
